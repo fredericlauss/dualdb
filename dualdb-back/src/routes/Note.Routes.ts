f6282e3bb77afedc702db77450cba6aa511bce94
frederic <flausson@normandiewebschool.fr>
2024-04-25 16:49:53 +0200

    rename parce que seb est pas content

--- dualdb-back/src/routes/Note.Routes.ts
+++ dualdb-back/src/routes/Note.Routes.ts
@@ -3,7 +3,7 @@ import { getNotes, findOne } from '../controllers/Note.controller';
 
 const router = express.Router();
 
-router.get('/getall', getNotes);
+router.get('/', getNotes);
 router.get('/:id', findOne);
 
 export default router;
