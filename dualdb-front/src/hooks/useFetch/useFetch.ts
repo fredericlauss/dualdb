76ae6e3d4941d147c64a2187b13a1adb4a032891
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2024-04-26 09:29:10 +0200

    Feat: useFetch hook

---
+++ dualdb-front/src/hooks/useFetch/useFetch.ts
@@ -0,0 +1,61 @@
+import { Request } from '@/services/api/helpers/request';
+import { API } from '@shared/api';
+import { useState, useEffect } from 'react';
+
+interface BaseResponse<T> extends API.Responses {
+    200: T
+}
+
+interface BaseRequest extends API.Request {
+    query: any
+}
+
+interface Configuration {
+    options: any
+    query: any
+}
+
+export const useFetch = <T, V extends API.Request>(
+        url: string, 
+        defaultData: T = ([] as T), 
+        configuration?: Configuration
+    ) => {
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [body, setBody] = useState<T>(defaultData);
+
+    const fetchFunction = async () => {
+        
+        setIsLoading(true);
+
+        try {
+            const response = await Request.get<BaseResponse<T>, BaseRequest>({
+                url,
+                query: configuration?.query,
+                options: configuration?.options,
+            });
+            
+            if (response.body)
+                setBody(response.body);
+            
+        } catch (error) {
+            console.log(`Error while fetching url ${url} :\n`, error);
+        }
+
+        setIsLoading(false);
+    }
+
+    const refresh = () => {
+        fetchFunction()
+    }
+
+    useEffect(() => {
+        fetchFunction()
+    }, [url]);
+
+    return {
+        body,
+        isLoading,
+        refresh
+    }
+}
