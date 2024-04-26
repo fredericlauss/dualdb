13571c51b219cf1a542f86033d8d13bf2f06cdd5
killian-lecorf <killianlecorf@outlook.fr>
2024-04-26 10:02:55 +0200

    add path on login

--- dualdb-back/src/controllers/User.controller.ts
+++ dualdb-back/src/controllers/User.controller.ts
@@ -46,9 +46,9 @@ export async function login(req: Request, res: Response) {
         return res.status(401).json({ message: 'Mot de passe incorrect' });
       }
   
-      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
   
-      res.cookie('jwt', token, { httpOnly: true, maxAge: 3600000 });
+      res.cookie('jwt', token, { httpOnly: true, maxAge: 3600000, path: "/" });
   
       res.status(200).json({ message: 'Authentification réussie' });
     } catch (error) {
